aa25a7aeb6e795393961c95a239adab8a746610d
Ernest Stasicki <ernest.stasicki@monogo.pl>
2020-02-05 10:32:18 +0100

    Simple form with simple classes

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,29 @@
     <link rel="stylesheet" type="text/css" href="css/styles.css">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>Example lorem ipsum</h1>
+    <div class="container">
+        <div class="main-form">
+            <form>
+                <div class="form-group">
+                    <label for="inputName">Imię i nazwisko</label>
+                    <input type="text" class="form-control" id="inputName">
+                </div>
+                <div class="form-group">
+                    <label for="inputEmail">Adres e-mail</label>
+                    <input type="email" class="form-control" id="inputEmail">
+                </div>
+                <div class="form-group">
+                    <label for="inputPhone">Numer telefonu</label>
+                    <input type="text" class="form-control" id="inputPhone">
+                </div>
+                <div class="d-flex align-items-center justify-content-center">
+                    <button type="submit" class="btn btn-primary">Wyślij formularz</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
